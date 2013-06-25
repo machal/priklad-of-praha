0a43a1ecb5f4992c83e72bbb22b33cb06ca3c1b1
Martin Michálek <michalek@shortcat.cz>
2013-06-25 06:49:49 +0200

    Uvodni commit

---
+++ assets/js/ovladni-frontend.js
@@ -0,0 +1,17 @@
+ /* Off-canvas navigace
+  * ========================= */
+
+$(document).ready(function() {
+
+  $('body').addClass('js');
+
+  var $menu = $('#side-menu'),
+    $menulink = $('.side-menu-link'),
+    $wrap = $('#wrap');
+
+  $menulink.click(function() {
+    $menulink.toggleClass('active');
+    $wrap.toggleClass('active');
+    return false;
+  });
+});
